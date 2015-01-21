f670912ef619a39fc32a7b0b05f548a64d83e09e
Maris Elsins <elmaris@gmail.com>
2015-01-21 09:58:24 +0200

    Adding the tablespace monitoring script
    The coolest tablespace monitoring scripts. Trust me!

---
+++ SQL/DB/tablespace_files.sql
@@ -0,0 +1,25 @@
+set pages 50000 lines 240 ver off
+col file_name for a80
+col ID for 999
+COLUMN DUMMY1 NOPRINT;
+COMPUTE SUM OF MAX_MB ON DUMMY1;
+COMPUTE SUM OF CURRENT_MB ON DUMMY1;
+BREAK ON DUMMY1;
+select 1 dummy1,
+       d.file_name file_name, 
+       d.file_id ID, 
+       d.tablespace_name, 
+       t.block_size, 
+       d.autoextensible, 
+       -- d.blocks blocks, 
+       d.blocks*t.block_size/1024/1024 current_mb, 
+       decode(d.autoextensible, 'YES', d.maxblocks, d.blocks) maxblocks, 
+       decode(d.autoextensible, 'YES', d.maxblocks, d.blocks)*t.block_size/1024/1024 max_mb, 
+       --d.increment_by increment_by, 
+       d.increment_by*t.block_size/1024/1024 increment_mb, 
+       d.status, 
+       d.online_status online_status
+ from dba_data_files d, dba_tablespaces t
+where t.tablespace_name like '%&1%'
+  and d.tablespace_name=t.tablespace_name
+order by d.tablespace_name, substr(d.file_name,instr('/',d.file_name,-1)+1);
---
+++ SQL/DB/tablespace_usage.sql
@@ -0,0 +1,32 @@
+set pages 50000 lines 200
+col bs for a3
+select tbl.tablespace_name,
+       tbl.bs,
+       tbl.current_df_size,
+       tbl.max_size,
+       tbl.used_size,
+       tbl.free_size,
+       round((tbl.free_size * 100) / tbl.max_size, 2) as free_prc,
+       round(((tbl.current_df_size - tbl.used_size) * 100) / tbl.current_df_size, 2) as free_prc_current_df_size
+  from (SELECT data.tablespace_name as tablespace_name, (select block_size/1024||'K' from dba_tablespaces a where a.tablespace_name=data.tablespace_name) bs,
+               round(maxsize.bytes / 1048576, 2) as max_size,
+               round(data.bytes / 1048576, 2) as current_df_size,
+               round((data.bytes  - free.bytes) / 1048576, 2) as used_size,
+               round((maxsize.bytes - (data.bytes - free.bytes)) / 1048576,2) as free_size
+          FROM (SELECT tablespace_name, SUM(BYTES) as bytes
+                  FROM dba_data_files
+                 WHERE tablespace_name NOT IN
+                       (select tablespace_name
+                          from dba_tablespaces
+                         WHERE contents = 'UNDO')
+                 GROUP BY tablespace_name) data,
+               (SELECT tablespace_name, SUM(NVL(BYTES, 0)) as bytes
+                  FROM dba_free_space
+                 GROUP BY tablespace_name) free,
+               (SELECT tablespace_name,
+                       SUM(greatest(bytes, maxbytes)) as bytes
+                  FROM dba_data_files
+                 GROUP BY tablespace_name) maxsize
+         WHERE data.tablespace_name = free.tablespace_name(+)
+           AND data.tablespace_name = maxsize.tablespace_name) tbl
+ order by 7 nulls first;
